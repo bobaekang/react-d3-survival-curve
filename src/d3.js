8bf69dd1d49334e74ea31de4133ec9d7edfefc2b
bobaekang <bkang.dev@gmail.com>
2020-06-19 16:47:58 -0500

    Reexport d3 modules as a single d3 module

---
+++ src/d3.js
@@ -0,0 +1,11 @@
+import * as d3Axis from 'd3-axis'
+import * as d3Scale from 'd3-scale'
+import * as d3Selection from 'd3-selection'
+import * as d3Shape from 'd3-shape'
+
+export default {
+  ...d3Axis,
+  ...d3Scale,
+  ...d3Selection,
+  ...d3Shape,
+}
