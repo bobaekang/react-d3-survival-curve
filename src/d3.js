8062f6df3dbfd2b63f7242905a7dfbb7db61c5d1
bobaekang <bkang.dev@gmail.com>
2020-06-22 10:33:31 -0500

    Add missing d3-scale-chromatic

--- src/d3.js
+++ src/d3.js
@@ -1,11 +1,13 @@
 import * as d3Axis from 'd3-axis'
 import * as d3Scale from 'd3-scale'
+import * as d3ScaleChromatic from 'd3-scale-chromatic'
 import * as d3Selection from 'd3-selection'
 import * as d3Shape from 'd3-shape'
 
 export default {
   ...d3Axis,
   ...d3Scale,
+  ...d3ScaleChromatic,
   ...d3Selection,
   ...d3Shape,
 }
